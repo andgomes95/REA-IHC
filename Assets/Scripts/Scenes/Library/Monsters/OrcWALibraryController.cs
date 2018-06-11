29181066b3b9999152539e440a439eead623c6f2
joaovcaetano <caetanosjoao@gmail.com>
2018-06-10 23:42:25 -0300

    Library scenes

---
+++ Assets/Scripts/Scenes/Library/Monsters/OrcWALibraryController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OrcWALibraryController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void backToMonsterLibrary(){
+		SceneManager.LoadScene("MonsterLibrary");
+	}
+}
